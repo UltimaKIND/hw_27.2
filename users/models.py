3321864fa051e9581ea62474eb1b36f8b5cdaebe
UltimaKIND <minion9@mail.ru>
2024-10-03 22:23:32 +0500

    созданы модели для приложений users, materials

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    """
+    Модель пользователя
+    """
+    username = None
+    email = models.EmailField(unique=True, verbose_name="почта")
+    phone = models.CharField(max_length=35, verbose_name="телефон", **NULLABLE)
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="аватар", **NULLABLE
+    )
+    city = models.CharField(max_length=50, verbose_name="город", **NULLABLE)
+    token = models.CharField(max_length=100, verbose_name="token", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
