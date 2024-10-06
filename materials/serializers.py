fd407ee75034bb5498e4d185eb9502bf8632a7f2
UltimaKIND <minion9@mail.ru>
2024-10-06 21:08:24 +0500

    в сериализатор Course добавлено поле вывода всех уроков курса

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,24 +2,6 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
-
-class CourseSerializer(ModelSerializer):
-    """
-    сериализатор модели курса
-    """
-    lessons = SerializerMethodField()
-
-    def get_lessons(self, course):
-        return Lesson.objects.all().filter(course=course).count()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
-
-
-
 class LessonSerializer(ModelSerializer):
     """
     сериализатор модели урока
@@ -28,3 +10,31 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+class CourseSerializer(ModelSerializer):
+    """
+    сериализатор модели курса
+    """
+
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    """
+    сериализатор модели курса
+    """
+    lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set',many=True, read_only=True)
+
+    def get_lessons_count(self, course):
+        return Lesson.objects.all().filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ['title', 'preview', 'description', 'lessons_count', 'lessons']
+
+
+
+
