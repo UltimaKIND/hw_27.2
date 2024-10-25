a708eea95d8aa8f54f381aa210cc017943b4c9b6
UltimaKIND <minion9@mail.ru>
2024-10-25 15:44:51 +0500

    описан Dockerfile и docker-compose.yml

--- config/settings.py
+++ config/settings.py
@@ -109,9 +109,11 @@ SIMPLE_JWT = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
